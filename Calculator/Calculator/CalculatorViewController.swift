48949797039423c9de825fb3455304a7c5ab07d9
chervonyi <yuri.ternopil@gmail.com>
2019-04-29 17:22:47 -0500

    Connect all UI components

--- Calculator/Calculator/CalculatorViewController.swift
+++ Calculator/Calculator/CalculatorViewController.swift
@@ -10,16 +10,84 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+    @IBOutlet var memorySlots: [UIButton]!
+    
+    @IBOutlet weak var labelResult: UILabel!
+    @IBOutlet weak var labelInput: UILabel!
+    
+    @IBOutlet weak var buttonOpenBrackets: UIButton!
+    @IBOutlet weak var buttonCloseBrackets: UIButton!
+    
+    @IBOutlet var buttonsNum: [UIButton]!
+    
+    @IBOutlet weak var buttonPoint: UIButton!
+    
+    @IBOutlet weak var buttonPercentage: UIButton!
+    @IBOutlet weak var buttonMultiplication: UIButton!
+    @IBOutlet weak var buttonPlus: UIButton!
+    @IBOutlet weak var buttonDivision: UIButton!
+    @IBOutlet weak var buttonMinus: UIButton!
+    @IBOutlet weak var buttonCalculation: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        memorySlots = memorySlots.sorted(by: {$0.tag < $1.tag})
+        buttonsNum = buttonsNum.sorted(by: {$0.tag < $1.tag})
+        
+        updateView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onClickCalculate(_ sender: UIButton) {
+        // TODO - Calculate
     }
-
-
+    
+    @IBAction func onClickErase(_ sender: UIButton) {
+        if let str = labelInput.text, str.count > 0 {
+            labelInput.text = String(str.dropLast())
+        }
+    }
+    
+    @IBAction func onClickOperator(_ sender: UIButton) {
+        
+        switch sender {
+        case buttonPercentage:      append(str: "%")
+        case buttonMultiplication:  append(str: "*")
+        case buttonPlus:            append(str: "+")
+        case buttonDivision:        append(str: "/")
+        case buttonMinus:           append(str: "-")
+        case buttonOpenBrackets:    append(str: "(")
+        case buttonCloseBrackets:   append(str: ")")
+        case buttonPoint:   append(str: ".")
+        default: break
+        }
+    }
+    
+    @IBAction func onClickNumber(_ sender: UIButton) {
+        append(str: String(sender.tag))
+    }
+    
+    func append(str: String) {
+        labelInput.text = labelInput.text! + str
+    }
+    
+    
+    func updateView() {
+        buttonOpenBrackets.layer.borderWidth = 2
+        buttonOpenBrackets.layer.borderColor = #colorLiteral(red: 0.2941176471, green: 0.3098039216, blue: 0.3254901961, alpha: 0.9469445634)
+        buttonOpenBrackets.layer.cornerRadius = 5
+        
+        buttonCloseBrackets.layer.borderWidth = 2
+        buttonCloseBrackets.layer.borderColor = #colorLiteral(red: 0.2941176471, green: 0.3098039216, blue: 0.3254901961, alpha: 0.9469445634)
+        buttonCloseBrackets.layer.cornerRadius = 5
+        
+        buttonPercentage.layer.cornerRadius =       buttonPercentage.layer.frame.width / 2
+        buttonMultiplication.layer.cornerRadius =   buttonMultiplication.layer.frame.width / 2
+        buttonPlus.layer.cornerRadius =             buttonPlus.layer.frame.width / 2
+        buttonDivision.layer.cornerRadius =         buttonDivision.layer.frame.width / 2
+        buttonMinus.layer.cornerRadius =            buttonMinus.layer.frame.width / 2
+        buttonCalculation.layer.cornerRadius =      buttonCalculation.layer.frame.height / 2
+    }
 }
 
